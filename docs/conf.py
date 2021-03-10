1455e493b771d5c0600ac0383c9cb66663f29feb
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-10 17:48:44 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,10 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
-import pkg_resources
-import datetime
+import sys # use sys 
+import os # use os
+import pkg_resources # use pkg_resources
+import datetime # use datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
