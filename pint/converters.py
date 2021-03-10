19a7424c1be2e44db683def84562de821840287b
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-10 17:52:15 +0300

    Update converters.py

--- pint/converters.py
+++ pint/converters.py
@@ -11,29 +11,34 @@
 from __future__ import (division, unicode_literals, print_function,
                         absolute_import)
 
-
+# create class Converter(object):
 class Converter(object):
     """Base class for value converters.
     """
 
     is_multiplicative = True
 
+    # create def to_reference(self, value, inplace=False):
     def to_reference(self, value, inplace=False):
         return value
 
+    # create def from_reference(self, value, inplace=False):
     def from_reference(self, value, inplace=False):
         return value
 
 
+# create class ScaleConverter(Converter):
 class ScaleConverter(Converter):
     """A linear transformation
     """
 
     is_multiplicative = True
 
+    # create def __init__(self, scale):
     def __init__(self, scale):
         self.scale = scale
 
+    # create def to_reference(self, value, inplace=False):
     def to_reference(self, value, inplace=False):
         if inplace:
             value *= self.scale
@@ -42,6 +47,7 @@ class ScaleConverter(Converter):
 
         return value
 
+    # create def from_reference(self, value, inplace=False):
     def from_reference(self, value, inplace=False):
         if inplace:
             value /= self.scale
@@ -50,19 +56,22 @@ class ScaleConverter(Converter):
 
         return value
 
-
+# create class OffsetConverter(Converter):
 class OffsetConverter(Converter):
     """An affine transformation
     """
 
+    # create def __init__(self, scale, offset):
     def __init__(self, scale, offset):
         self.scale = scale
         self.offset = offset
 
+    # create @property
     @property
     def is_multiplicative(self):
         return self.offset == 0
 
+    # create def to_reference(self, value, inplace=False):
     def to_reference(self, value, inplace=False):
         if inplace:
             value *= self.scale
@@ -70,8 +79,9 @@ class OffsetConverter(Converter):
         else:
             value = value * self.scale + self.offset
 
-        return value
+        return value 
 
+    # create def from_reference(self, value, inplace=False):
     def from_reference(self, value, inplace=False):
         if inplace:
             value -= self.offset
